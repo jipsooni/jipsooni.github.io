bf4f5efeb126a3751205d75e0bb545a7e86ea6dc
Lena Kislitcyn <kislitcyn.lena@gmail.com>
2022-10-04 02:04:07 +0200

    Implement dark mode

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let toggle = document.querySelector("dark-mode-toggle")
-toggle.addEventListener("click", () => {
-     if (toggle.checked) {
-          document.body.style.setProperty("background", "#2d334a")
-     } else {
-          document.body.style.setProperty("background", "#fffffe")
+class CssPropControl {
+     constructor(element) {
+          this.element = element
      }
+     get(varName) {
+          return getComputedStyle(this.element).getPropertyValue(varName)
+     }
+     set(varName, val) {
+          return this.element.style.setProperty(varName, val)
+     }
+}
+
+const bodyCssProps = new CssPropControl(document.body)
+
+let toggle = document.querySelector("#dark-mode-toggle")
+toggle.addEventListener("click", () => {
+     let mode = toggle.checked ? "dark" : "light"
+     bodyCssProps.set("--background", bodyCssProps.get(`--${mode}-background`))
+     bodyCssProps.set("--primary", bodyCssProps.get(`--${mode}-primary`))
+     bodyCssProps.set("--link", bodyCssProps.get(`--${mode}-link`))
+
 })
